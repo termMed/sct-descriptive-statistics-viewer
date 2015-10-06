3451f4681b0234e9412876e4492240b3e2bcc53f
alopezo <alopez@termmed.com>
2015-10-06 18:58:10 -0300

    test2 working

---
+++ material/views/compiled/templates.js
@@ -0,0 +1,10 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["material/views/home.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<div class=\"col-md-6\">\n    <div class=\"card\">\n        <div class=\"card-height-indicator\"></div>\n        <div class=\"card-content\">\n            <div class=\"card-body\">\n                <h1>SNOMED CT Descriptive Statistics</h1>\n                <p class=\"lead\">For 20160131 Release</p>\n                <p><em>SNOMED CT Release Descriptive Statistics</em> is a demo site for an IHTSDO Quality Assurance project.</p>\n                <br><br><br>\n                <p><i class=\"mdi-alert-warning\"></i>This is not real data. Contact the <a href=\"www.ihtsdo.org\">IHSTDO</a> for more information.</p>\n            </div>\n            <footer class=\"card-footer\">\n                <!--<button class=\"btn btn-flat\">Share</button>-->\n                <!--<button class=\"btn btn-flat btn-warning\">Learn More</button>-->\n            </footer>\n        </div>\n    </div>\n</div>\n";
+  });
